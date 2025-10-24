2ba4ae2e0e7d40e75fc015dc39bb5ebb59a330a2
luckypenny1632333 <stevenpiao0822@gmail.com>
2025-10-24 11:19:14 +0300

    [feat] updated the resume and linkedin profile link

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -2,7 +2,7 @@ import myImage from "/public/images/Mark.png"
 
 export const AvatarImage: string = "/public/images/Mark.png"
 export const homeProfileImage = myImage
-export const cvURL: string = "https://drive.google.com/file/d/1Ww4iVsTDp_KTPgadwF_KJFudFcTd22rA/view"
+export const cvURL: string = "https://drive.google.com/file/d/1-GRD83e6c5SxR3ScEhtsxHkHz8lPJDTF/view?usp=sharing"
 
 export const navigationRoutes: string[] = [
   "home",
